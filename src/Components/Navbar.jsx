0782e8cd27f4f4b435a16a3b86fadc03facfdcd4
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-01-11 17:11:10 +0530

    added Navbar for large screen size

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Hamburger from 'hamburger-react';
+
+const  Navbar=()=> {
+   
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="lg:h-20 h-16 flex items-center justify-between shadow-[#1026490F]/10  shadow-md bg-[#FFFFFF]  w-screen">
+      <div>
+        <img src='KoinXLogo.png' alt="Logo" className="w-24 h-6 lg:ml-14 ml-4"></img>
+      </div>
+      {/* For larger screens like laptop dekstop i will make this responsiveness */}
+      <div className="hidden lg:flex items-center">
+        <div className="text-lg font-semibold text-[#0F1629] mr-6">
+          Crypto Taxes
+        </div>
+        <div className="text-lg font-semibold text-[#0F1629] mr-6">
+          Free Tools{" "}
+        </div>
+        <div className="text-lg font-semibold text-[#0F1629] mr-6">
+          Resource Center{" "}
+        </div>
+        <div>
+          <button className="bg-gradient-to-r from-[#2870EA] to-[#1B4AEF] px-8 py-2 text-white rounded-lg mx-14">
+            Get Started
+          </button>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Navbar;
